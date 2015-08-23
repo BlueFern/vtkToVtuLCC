479e358681ad1331b0092d9875feeb4e4fc6c79c
Constantine Zakkaroff <constantine.zakkaroff@canterbury.ac.nz>
2015-08-23 19:51:49 +1200

    Style corrections

--- util/ConvertAllToVtu.py
+++ util/ConvertAllToVtu.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug  5 12:33:31 2015
-
-@author: sed59
+Batch script to convert a directory of vtk files to vtu files.
 """
 import sys
 import glob
 import os
 
-
 def main():
-    
     if len(sys.argv) != 2:
-        sys.exit("Requires directory of vtk files")
+        sys.exit("Expected argument: <directory containing vtk files>")
     
     if not os.path.isfile("ConvertVtkToVtu"):
         sys.exit("Provide a link to ConvertVtkToVtu in current directory")
         
-    base_files = glob.glob(sys.argv[1] + "/*.vtk")
-    for filename in base_files:
-        output_file = filename[:-1] + 'u'
-        args = "./ConvertVtkToVtu " + filename + " " + output_file
+    input_files = glob.glob(sys.argv[1] + "/*.vtk")
+    for in_file in input_files:
+        out_file = in_file[:-1] + 'u'
+        args = "./ConvertVtkToVtu " + in_file + " " + out_file
         os.system(args)
 
 if __name__ == "__main__":
